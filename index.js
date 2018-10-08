edf42bdcc4e57ab89abea9e5fa3852e0616d0f5a
Anandha Sudhan <rasudhan@gmail.com>
2018-10-08 22:53:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 $(document).ready(function (){
 
+  var searchRepositories = () => {
+    const searchTerms = $('#searchTerms').val()
+    $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+        $('#results').html(renderSearchResults(data))
+      }).fail(error => {
+        displayError()
+      })
+  }
+
+  var renderSearchResults = (data) => data.items.map( result => renderSearchResult(result))
+
+  var renderSearchResult = (result) => {
+    return `
+        <div>
+          <h2><a href="${result.html_url}">${result.name}</a></h2>
+          <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+          <p>${result.description}</p>
+        </div>
+        <hr>
+      `
+  }
+
+
+  
   var displayError = () => $('#errors').html("I'm sorry, there's been an error. Please try again.")
   
   var renderCommit = (commit) => {
@@ -19,26 +43,8 @@ $(document).ready(function (){
     })
   }
   
-  var renderSearchResult = (result) => {
-    return `
-        <div>
-          <h2><a href="${result.html_url}">${result.name}</a></h2>
-          <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
-          <p>${result.description}</p>
-        </div>
-        <hr>
-      `
-  }
   
-  var renderSearchResults = (data) => data.items.map( result => renderSearchResult(result))
   
-  var searchRepositories = () => {
-    const searchTerms = $('#searchTerms').val()
-    $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
-        $('#results').html(renderSearchResults(data))
-      }).fail(error => {
-        displayError()
-      })
-  }
+
   
 });
